8fd472c64aca15c1d657b551e22a895e4def9480
luizfsnunes <luizfsnunes@gmail.com>
2018-05-29 22:05:24 -0300

    Adicionar sessão e dados de usuário logado

--- src/Infrastructure/Controller/AbstractController.php
+++ src/Infrastructure/Controller/AbstractController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace Infrastructure\Controller;
 
 use Atlas\Orm\Atlas;
+use Aura\Auth\Auth;
+use Aura\Session\Session;
 use Infrastructure\Service\Renderer\TwigRenderer;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Response;
@@ -21,14 +23,27 @@ class AbstractController
     protected $renderer;
 
     /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var Auth
+     */
+    protected $auth;
+
+    /**
      * AbstractController constructor.
      * @param TwigRenderer $renderer
      * @param Atlas $atlas
+     * @param Auth $auth
      */
-    public function __construct(TwigRenderer $renderer, Atlas $atlas)
+    public function __construct(TwigRenderer $renderer, Atlas $atlas, Auth $auth, Session $session)
     {
         $this->renderer = $renderer;
         $this->atlas = $atlas;
+        $this->auth = $auth;
+        $this->session = $session;
     }
 
     /**
@@ -57,5 +72,21 @@ class AbstractController
         $response = new Response();
         $response = $response->withJson($data, $status);
         return $response;
+    }
+
+    /**
+     * @return Auth
+     */
+    public function getAuth(): Auth
+    {
+        return $this->auth;
+    }
+
+    /**
+     * @return Session
+     */
+    public function getSession(): Session
+    {
+        return $this->session;
     }
 }
